b81dc301c587ad5fbc546ebd4d7b63d5b95e90fd
DMYTRO BORIICHUK <dmytro.boriichuk@dxc.com>
2023-02-08 11:45:49 +0200

    init commit

---
+++ source/main.py
@@ -0,0 +1,22 @@
+import argparse
+from pathlib import Path
+from datetime import datetime
+
+# Set output folder relatively to script location
+output_path_folder = Path(__file__).parent / ".." / "Output"
+output_path_folder = output_path_folder.resolve()
+print(output_path_folder)
+
+parser = argparse.ArgumentParser(
+    prog="Output test to file"
+)
+
+parser.add_argument("-t", "--text", type=str)
+args = parser.parse_args()
+
+
+# Prepare outpupt file name
+output_file_path = output_path_folder/ f'result-{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.txt'
+print(output_file_path)
+with open(output_file_path, "w") as new_file:
+    new_file.write(args.text)
