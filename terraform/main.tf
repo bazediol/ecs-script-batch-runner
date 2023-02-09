4fd852482ec9e9c63c7f4d601f0e789d36d65582
DMYTRO BORIICHUK <dmytro.boriichuk@dxc.com>
2023-02-09 13:31:18 +0200

    Add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "personal"
+}
+
+resource "aws_ecs_cluster" "terraform_cluster" {
+  name = "terraform-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "disabled"
+  }
+
+  tags = {
+    app = "terraform"
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "terraform_cluster_providers" {
+  cluster_name = aws_ecs_cluster.terraform_cluster.name
+
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+
+}
+
+resource "aws_ecs_task_definition" "terraform_task" {
+  family = "Terraform-task"
+  cpu = 1024
+  memory = 2048
+
+  container_definitions = <<TASK_DEFINITION
+  [
+    {
+        "name": "script-runner",
+        "image": "alpine:3.17",
+        "cpu": 0,
+        "portMappings": [
+            {
+                "name": "script-runner-80-tcp",
+                "containerPort": 80,
+                "hostPort": 80,
+                "protocol": "tcp",
+                "appProtocol": "http"
+            }
+        ],
+        "essential": true,
+        "environment": [],
+        "environmentFiles": [],
+        "mountPoints": [],
+        "volumesFrom": [],
+        "command": ["pip", "install", "-r", "requirements.txt"]
+    }
+  ]
+  TASK_DEFINITION
+  
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  tags = {
+    app = "terraform"
+  }
+
+}
